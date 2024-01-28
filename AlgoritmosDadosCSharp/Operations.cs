216e6ded90eb63c75ac319ffd48d7648fb72682b
diegorafael <diegorafael_si@msn.com>
2024-01-28 20:03:10 +0000

    fix: Allowing deterministic results to test BatataQuente's implementation; fix: Make TenOrMoreChars a pure function docs: Add comments to implementations

--- AlgoritmosDadosCSharp/Operations.cs
+++ AlgoritmosDadosCSharp/Operations.cs
@@ -2,21 +2,26 @@
 
 public class OperacoesListas
 {
+    // Está 100% funcional. Entretanto, em termos de manutenibilidade, é recomendado que 
+    // não alteremos o objeto de entrada pois isso acaba causando indireção. Isso é chamado de
+    // fnução mutadora ou função destrutiva.
+    // O ideal é criar uma nova lista e colocar nela os objetos resultantes da "filtragem", tornando a
+    // função imutável.
+    // Note que os objetos não serão duplicados na memória, eles só estarão sendo referenciados
+    // em duas listas diferentes =)
     public static List<string> TenOrMoreChars(List<string> input)
     {
-        for (var index = 0; index < input.Count;)
+        var result = new List<string>();
+
+        for (var index = 0; index < input.Count; index++)
         {
-            if (input[index].Length < 10)
+            if (input[index].Length >= 10)
             {
-                input.RemoveAt(index);
-            }
-            else
-            {
-                index++;
+                result.Add(input.ElementAt(index));
             }
         }
 
-        return input;
+        return result;
     }
 }
 
@@ -45,26 +50,43 @@ public class OperacoesPilhas
     }
 }
 
+public interface IRandomizer
+{
+    int Next(int minValue, int maxValue);
+}
+
+public class NativeRandomizer : IRandomizer
+{
+    Random rnd = new Random();
+    public int Next(int minValue, int maxValue)
+        => rnd.Next(minValue, maxValue);
+}
+
 public class OperacoesFilas
 {
+
     public static int BatataQuente(int numJogadores)
+        => BatataQuente(new NativeRandomizer(), numJogadores);
+
+    public static int BatataQuente(IRandomizer randomizer, int numJogadores)
     {
         // Test cases
         if (numJogadores <= 1)
             throw new ArgumentOutOfRangeException(nameof(numJogadores),
+                // Nice catch! Se quisesse garantir isso em tempo de compilação, poderia
+                // também definir o parametro com o tipo `uint`, que seria o inteiro
+                // sem sinal, fazendo com que número fosse positivo
                 "Numero de jogadores deve ser positivo e maior que um");
-
-        var passesRandom = new Random();
 
         var FilaJogadores = new Queue<int>();
 
         // Coloca os Jogadores em seus postos
         for (var i = 1; i <= numJogadores; i++) FilaJogadores.Enqueue(i);
-
+        
         // Jogo
         while (FilaJogadores.Count > 1)
         {
-            var passes = passesRandom.Next(1, 100);
+            var passes = randomizer.Next(1, 100);
 
             // Passar a batata dentre jogadores
             for (var i = 0; i < passes; i++)
--- Tests/XunitTests.cs
+++ Tests/XunitTests.cs
@@ -88,18 +88,42 @@ public class OperacoesPilhasTests
     }
 }
 
-public class OperacoesPilhaTests
+public class OperacoesFilasTests
 {
-    [Theory]
-    [InlineData(3)]
-    [InlineData(5)]
-    public void BatataQuente_RetornaResultadoValido(int numJogadores)
+    private class DeterministicRandomizer : IRandomizer
     {
+        private readonly int _presetResult;
+
+        public DeterministicRandomizer(int presetResult)
+        {
+            _presetResult = presetResult;
+        }
+
+        public int Next(int minValue, int maxValue)
+        {
+            return _presetResult;
+        }
+    }
+
+    [Theory]
+    [InlineData(3, 9, 2)]
+    [InlineData(5, 19, 3)]
+    // O resultado de um teste automatizado deve ser determinístico.
+    // Para isso temos que ter no nosso sistema
+    // classes testáveis, isto é, as classes devem de alguma forma permitir o resultado esperado para determinados
+    // cenários sejam coletáveis.
+    // Nesse caso, precisei fazer algumas modificações para permitir injetar o "randomizador" e definir o número de
+    // passes, pois assim consigo predizer o jogador vencedor com um simples teste de mesa.
+    public void BatataQuente_RetornaResultadoValido(int numJogadores, int numPasses, int vencedorEsperado)
+    {
+        // Arrange
+        var randomizer = new DeterministicRandomizer(numPasses);
+
         // Act
-        var result = OperacoesFilas.BatataQuente(numJogadores);
+        var result = OperacoesFilas.BatataQuente(randomizer, numJogadores);
 
         // Assert
-        Assert.InRange(result - 1, 0, numJogadores - 1);
+        Assert.Equal(result, vencedorEsperado);
     }
 
     [Theory]
@@ -122,7 +146,8 @@ public class OperacoesPilhaTests
         var result = OperacoesFilas.BatataQuente(numJogadores);
 
         // Assert
-        Assert.True(result >= 0 && result <= numJogadores);
+        // 0 (zero) não deveria estar dentro do espectro válido XP
+        Assert.True(result >= 1 && result <= numJogadores);
     }
 
     [Theory]
@@ -135,12 +160,34 @@ public class OperacoesPilhaTests
         var result = OperacoesFilas.BatataQuente(numJogadores);
 
         // Assert
-        Assert.InRange(result, 0, numJogadores - 1);
+        // Do jeito que estava antes, esse teste poderia resultar em um falso positivo.
+        // Os jogadores válidos são entre 1 e numJogadores.
+        // Se houvesse um erro de implementação e sua classe do jogo retornasse 0 (zero), esse teste aceitaria
+        // como resultado válido esperado
+        Assert.InRange(result, 1, numJogadores - 1);
     }
 }
 
 public class OperacoesDictTests
 {
+    const string loremIpsum = @"Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Nulla auctor porta velit a tincidunt. Nam efficitur iaculis placerat. Aenean lectus dui, 
+        sollicitudin id rhoncus tristique, aliquet sed quam. Phasellus blandit magna at elementum 
+        consequat. Nam vitae nunc vehicula, blandit felis a, placerat augue. Quisque bibendum a 
+        ipsum at scelerisque. Duis molestie turpis quis orci vehicula aliquam. Duis non elementum 
+        erat. Phasellus et dui odio. Nunc vitae leo sem. Curabitur nec enim id mi aliquet commodo 
+        at et sapien. Fusce sit amet nisi elit. Interdum et malesuada fames ac ante ipsum primis 
+        in faucibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere 
+        cubilia curae; Duis vitae dolor at sem ultrices euismod. Morbi aliquet, felis et mattis 
+        congue, justo nunc pharetra lectus, a lobortis mauris eros et nulla. Orci varius natoque 
+        penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas sollicitudin 
+        posuere nibh malesuada suscipit. Nam a sapien ex. Donec mollis justo est, quis tempus mi 
+        pharetra at. Cras fringilla enim eu egestas scelerisque. Praesent tristique imperdiet 
+        consectetur. Donec interdum pulvinar nulla vel pharetra. Class aptent taciti sociosqu ad 
+        litora torquent per conubia nostra, per inceptos himenaeos. Aliquam interdum finibus mi, 
+        in tempus lorem. Cras diam magna, viverra vitae ante eget, scelerisque sodales velit. 
+        Aliquam erat volutpat. Mauris consectetur sapien mi, vel euismod quam consectetur id.";
+
     [Theory]
     [InlineData("to be or not to be, that is the question")]
     public void ContadorPalavras_RetornaNumeroExato(string text)
@@ -157,8 +204,19 @@ public class OperacoesDictTests
         Assert.Equal(1, result["is"]);
         Assert.Equal(1, result["the"]);
         Assert.Equal(1, result["question"]);
+    }
 
+    [Theory]
+    [InlineData(loremIpsum)]
+    public void ContadorPalavras_TestePorAmostra(string text)
+    {
+        // Act
+        var result = OperacoesDict.ContadorPalavras(text);
 
-
+        // Assert
+        Assert.Equal(7, result["et"]);
+        Assert.Equal(2, result["lorem"]);
+        Assert.Equal(4, result["ipsum"]);
+        Assert.Equal(1, result["viverra"]);
     }
 }
